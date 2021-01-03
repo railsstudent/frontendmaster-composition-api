5e6f12454f44d65451902f5f71178ca9476284c2
Connie Leung <cleung334@gmail.com>
2021-01-03 11:29:46 +0800

    feat: composition api in nuxt
    Signed-off-by: Connie Leung <cleung334@gmail.com>

---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+    <div>
+      <p>Count: {{ count }}</p>
+      <br />
+      <button class="button--green" @click="addToCount(-1)">Decrement</button>
+      <button class="button--green" @click="addToCount(1)">Increment</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const count = ref(0)
+
+    function addToCount(delta: number) {
+      count.value = count.value + delta
+    }
+
+    return {
+      count,
+      addToCount,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+</style>
